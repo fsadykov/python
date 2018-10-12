d3c416d30ae371df71654ec6e40c8594fb44aa8b
farkhodsadykov <sadykovfarkhod@gmail.com>
2018-10-12 00:43:36 -0500

    Comoany task #4 is Done

--- Company/Task_4/distancemetrix.py
+++ Company/Task_4/distancemetrix.py
@@ -9,28 +9,33 @@ with open('Kiosk Coords.csv', 'r') as f:
     kiosks = list(reader)
     kiosks.pop(0)
 
-address = []
-for item in kiosks:
-    address.append([item[0], item[1]])
-# I have now name of the building and address of the building
-# ['600 W Chicago', '600 W Chicago Ave, Chicago, IL 60654']
 
-def nextStop(addreslist, curentLocation):
-    global allAddress
+address = []
+address.append(beginDest)
+for item in kiosks:
+    address.append(item[1])
+
+
+
+def nextStop(addresslist, curentLocation):
     allAddress = []
-    for addres in addreslist:
-        response = requests.get(url + curentLocation + ',DC&destinations=' + addres[1] + ' &key=AIzaSyCGvB2cNSP1Lhj6LiXtq13DtUagbHSvry8')
+    for addres in addresslist:
+        response = requests.get(url + curentLocation + ',DC&destinations=' + addres + ' &key=AIzaSyCGvB2cNSP1Lhj6LiXtq13DtUagbHSvry8')
         responseDict = response.json()
         mile = str(responseDict['rows'][0]['elements'][0]['distance']['text'])
-        addres = responseDict['origin_addresses'][0]
+        addres = responseDict['destination_addresses'][0]
         allAddress.append([mile, addres])
         allAddress.sort()
-        driverGo = allAddress.pop(0)
-    return driverGo
+        result = allAddress[0]
+    return result
 
-def driver():
-    for address in allAddress:
-        nextStop(allAddress, driverGo)
+
+for item in address:
+    nextAddress = nextStop(address, address[0])
+    print("Your next stop is :", nextAddress)
+    address.pop(address.count(nextStop))
+
+
 
 if __name__ == '__main__':
-    driver()
+    main()
